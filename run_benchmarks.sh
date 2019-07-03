bb8858d3204dc8b9106a0712b5edff141b4de49b
David Justen <mail@davidjusten.de>
2019-07-03 17:16:21 +0200

    ..

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -36,19 +36,19 @@ mv out.csv ../visualization/int_10m_multi_5m_distinct.csv
 
 # String Single-Threaded
 ./src/Main 100000 string st 1000 1000 leq
-mv out.csv ../visualization/int_100k_single_1k_distinct.csv
+mv out.csv ../visualization/string_100k_single_1k_distinct.csv
 ./src/Main 100000 string st 1000 50000 leq
-mv out.csv ../visualization/int_100k_single_50k_distinct.csv
+mv out.csv ../visualization/string_100k_single_50k_distinct.csv
 ./src/Main 1000000 string st 1000 1000 leq
-mv out.csv ../visualization/int_1m_single_1k_distinct.csv
+mv out.csv ../visualization/string_1m_single_1k_distinct.csv
 ./src/Main 1000000 string st 1000 500000 leq
-mv out.csv ../visualization/int_1m_single_500k_distinct.csv
+mv out.csv ../visualization/string_1m_single_500k_distinct.csv
 ./src/Main 10000000 string st 1000 1000 leq
-mv out.csv ../visualization/int_10m_single_1k_distinct.csv
+mv out.csv ../visualization/string_10m_single_1k_distinct.csv
 ./src/Main 10000000 string st 1000 1000000 leq
-mv out.csv ../visualization/int_10m_single_1m_distinct.csv
+mv out.csv ../visualization/string_10m_single_1m_distinct.csv
 ./src/Main 10000000 string st 1000 5000000 leq
-mv out.csv ../visualization/int_10m_single_5m_distinct.csv
+mv out.csv ../visualization/string_10m_single_5m_distinct.csv
 
 # String Multi-Threaded
 numactl -N2 -m2 ./src/Main 100000 string mt 1000 1000 leq
